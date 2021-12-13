9d5a7f85b9265818fb297807f3d759ecd3a19043
Andrew Chernigovsky <chernigovsky108@gmail.com>
2021-12-13 18:48:30 +0300

    js tabs

--- source/js/tabs.js
+++ source/js/tabs.js
@@ -1,19 +1,90 @@
-let tabsTours = document.querySelectorAll('.aboutTours__link');
-let content = document.querySelectorAll('.aboutTours__content');
+const tabsTours = document.querySelectorAll('.aboutTours__link');
+const contentTours = document.querySelectorAll('.aboutTours__content');
+const contentWrapperTours = document.querySelectorAll('.aboutTours__content-wrapper');
+const bodyTours = document.querySelector('.aboutTours__body');
+const bodyWrapperTours = document.querySelector('.aboutTours__body-wrapper');
+const headerTours = document.querySelectorAll('.aboutTours__tab');
 
 for (let i = 0; i < tabsTours.length; i++) {
   tabsTours[i].addEventListener('click', () => tabClick(i));
 }
 
 function tabClick(currentTab) {
-    removeActive();
-    tabsTours[currentTab].classList.add('active');
-    content[currentTab].classList.add('active');
+  removeActive();
+  tabsTours[currentTab].classList.add('active');
+  contentTours[currentTab].classList.add('active');
 }
 
 function removeActive() {
     for (let i = 0; i < tabsTours.length; i++) {
         tabsTours[i].classList.remove('active');
-        content[i].classList.remove('active');
+        contentTours[i].classList.remove('active');
     }
+}
+
+for (let i = 0; i < tabsTours.length; i++) {
+  contentTours[i].addEventListener('touchstart', handleTouchStart, false);
+  contentTours[i].addEventListener('touchmove', handleTouchMove, false);
+}
+
+let x1 = null;
+let y1 = null;
+
+let width;
+let count = 0;
+
+function handleTouchStart(evt) {
+  const firstTouch = evt.touches[0];
+
+  x1 = firstTouch.clientX;
+  y1 = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+
+  if (!x1 || !y1){
+    return false;
+  }
+
+  let x2 = evt.touches[0].clientX;
+  let y2 = evt.touches[0].clientY;
+  let xDiff = x2 - x1;
+  let yDiff = y2 - y1;
+
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) {
+      count++;
+      if (count >= contentTours.length) {
+          count = 0;
+      }
+      swipeTour();
+    } else {
+        count--;
+        if (count < 0) {
+            count = contentTours.length - 1;
+        }
+        swipeTour();
+      }
+    }
+  x1 = null;
+  y1 = null;
+}
+
+function swipeTour(){
+
+  width = bodyTours.offsetWidth;
+  bodyWrapperTours.style.width = width * contentTours.length + 'px';
+
+  contentTours.forEach(item => {
+    item.style.width = width +'px';
+    item.style.height = 'auto';
+  });
+
+  SwipeAnimate();
+  window.addEventListener('resize', swipeTour);
+}
+
+ function SwipeAnimate() {
+  bodyWrapperTours.style.transform = 'translate(-' + count * width + 'px)';
+
 }
